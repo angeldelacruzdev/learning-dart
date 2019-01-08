a7eda801f322365a9a8dad6eda23c0b73f530dc0
Angel De La Cruz <angeldelacruzdev@gmail.com>
2019-01-08 16:02:18 -0400

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,21 @@
+void main() {
+
+//  for(var i = 0; i < 4; i++){
+//      print("Hello");
+//  }
+//
+ var i = 0;
+//  while( i < 4){
+//    print("Hello");
+//    i++;
+//  }
+
+
+
+ do {
+   print("Hello world");
+   i++;
+ }while(i < 4);
+
+}
+
---
+++ test/dart_course_test.dart
@@ -0,0 +1,6 @@
+import 'package:dart_course/dart_course.dart';
+import 'package:test/test.dart';
+
+void main() {
+   print("Hello world");
+}
