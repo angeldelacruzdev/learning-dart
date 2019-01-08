bd38066b24153f9a3337adafbadb8079bec1c535
Angel De La Cruz <angeldelacruzdev@gmail.com>
2019-01-08 17:52:29 -0400

    functions, methods and parameters

--- bin/main.dart
+++ bin/main.dart
@@ -1,21 +1,13 @@
 void main() {
 
-//  for(var i = 0; i < 4; i++){
-//      print("Hello");
-//  }
-//
- var i = 0;
-//  while( i < 4){
-//    print("Hello");
-//    i++;
-//  }
+  findPerimeter(2 ,4);
+  int area = getArea(5, 8);
 
-
-
- do {
-   print("Hello world");
-   i++;
- }while(i < 4);
-
+  print(area);
 }
 
+void findPerimeter( int length, int breadth) => print("The perimeter is: ${2  * (length +  breadth) }");
+//return area
+int getArea(int length, int breadth) => length * breadth;
+
+//https://www.youtube.com/watch?v=pH-CP8s_xK8&list=PLlxmoA0rQ-LyHW9voBdNo4gEEIh0SjG-q&index=21
